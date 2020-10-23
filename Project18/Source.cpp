317566028a85a7030f96dee03f4a864aa19c8fe6
NATTANICHA SENTHA <63010299@KMITL.AC.TH>
2020-10-23 22:35:59 +0700

    Add project files.

---
+++ Project18/Source.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int letters(char input[])
+{
+
+	int i = 0, sum = 0;
+	{
+		while (input[i] != '\0')
+		{
+			if (input[i] == 'A' )
+			{
+				if (input[i + 1] == 'O')
+				{
+					if (input[i + 2] == 'M')
+					{
+						sum++;
+					}
+				}
+			}
+			i++;
+		}
+	}
+	return sum ;
+}
+int main()
+{
+	char input[1000];
+	scanf("%s", &input);
+	printf("Letters = %d", letters(input));
+	return 0;
+}
